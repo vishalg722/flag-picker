4770ffefb1ae573f5dd827f680251ccd495ba26e
Vishal Gupta <vishalgupta2@Vishals-MacBook-Pro.local>
2021-02-10 18:22:01 +0530

    Add Flag picker project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import SearchComponent from './SearchComponent';
+import { getCountries } from './utils/util'
+
 import './App.css';
 
 function App() {
+
+  const [ continent , setContinent] = useState(false);
+  
+  const setContinentHandler = (continent) => {
+    setContinent(continent)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1> Flag Picker</h1>
+        <p className="pickerText"> This app will help to learn flags arround the world in <span className="steps">3 steps</span>.</p>
+      </div>
+      <div className='content'>
+          <SearchComponent setContinentHandler={ setContinentHandler }  />
+      </div>
     </div>
   );
 }
